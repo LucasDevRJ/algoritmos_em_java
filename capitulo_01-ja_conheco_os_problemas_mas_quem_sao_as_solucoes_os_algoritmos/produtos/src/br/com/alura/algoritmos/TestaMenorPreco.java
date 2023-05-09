b1c111b71ca0b43aeb2f16864a09383e631e7d50
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-09 15:53:28 -0300

    Estrutura de repetição para armazenar o carro mais barato e exibi-lo

--- capitulo_01-ja_conheco_os_problemas_mas_quem_sao_as_solucoes_os_algoritmos/produtos/src/br/com/alura/algoritmos/TestaMenorPreco.java
+++ capitulo_01-ja_conheco_os_problemas_mas_quem_sao_as_solucoes_os_algoritmos/produtos/src/br/com/alura/algoritmos/TestaMenorPreco.java
@@ -9,5 +9,18 @@ public class TestaMenorPreco {
 		precos[2] = 16000;
 		precos[3] = 46000;
 		precos[4] = 17000;
+		
+		int maisBarato = 0;
+		int atual = 0;
+		
+		for (int i = 0; i < 4; i++) {
+			if (precos[atual] < precos[maisBarato]) {
+				maisBarato = atual;
+			}
+			atual = atual + 1;
+		}
+		
+		System.out.println("Indice do carro mais barato: " + maisBarato);
+		System.out.println("Preço do carro mais barato: R$ " + precos[maisBarato]);
 	}
 }
