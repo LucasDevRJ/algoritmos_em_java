cfd872f9e325e2cd905e01e0ade4d778a3c90e8b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-09 17:03:15 -0300

    Refatoração: deixando a função para achar o carro mais barato generica

--- capitulo_01-ja_conheco_os_problemas_mas_quem_sao_as_solucoes_os_algoritmos/produtos/src/br/com/alura/algoritmos/TestaMenorPreco.java
+++ capitulo_01-ja_conheco_os_problemas_mas_quem_sao_as_solucoes_os_algoritmos/produtos/src/br/com/alura/algoritmos/TestaMenorPreco.java
@@ -8,19 +8,23 @@ public class TestaMenorPreco {
 			new Produto("Jipe", 46000),
 			new Produto("Brasília", 16000),
 			new Produto("Smart", 46000),
-			new Produto("Fusca", 17000)
+			new Produto("Fusca", 17000),
+			null,
+			null,
+			null,
+			null,
+			null
 		};
 		
-		int maisBarato = buscaMenor(produtos);
+		int maisBarato = buscaMenor(produtos, 0, 4);
 		
 		System.out.println(maisBarato);
 		System.out.println("O carro " + produtos[maisBarato].getNome() + " é o mais barato, e custa " + produtos[maisBarato].getPreco());
 	}
 	
-	private static int buscaMenor(Produto[] produtos) {
-		int maisBarato = 0;
-		int termino = produtos.length - 1;
-		for (int atual = 0; atual <= termino; atual++) {
+	private static int buscaMenor(Produto[] produtos, int inicio, int termino) {
+		int maisBarato = inicio;
+		for (int atual = inicio; atual <= termino; atual++) {
 			if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
 				maisBarato = atual;
 			}
