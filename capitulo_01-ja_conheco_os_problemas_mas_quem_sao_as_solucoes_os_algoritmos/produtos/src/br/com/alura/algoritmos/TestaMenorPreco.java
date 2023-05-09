d0fbf8158769822b9f1132020f404bb63b8d27b0
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-09 15:43:58 -0300

    Criação do array para armazenar cinco preços de carros

---
+++ capitulo_01-ja_conheco_os_problemas_mas_quem_sao_as_solucoes_os_algoritmos/produtos/src/br/com/alura/algoritmos/TestaMenorPreco.java
@@ -0,0 +1,13 @@
+package br.com.alura.algoritmos;
+
+public class TestaMenorPreco {
+
+	public static void main(String[] args) {
+		double precos[] = new double[5];
+		precos[0] = 1000000;
+		precos[1] = 46000;
+		precos[2] = 16000;
+		precos[3] = 46000;
+		precos[4] = 17000;
+	}
+}
