fbd256a1651e428f5ef8a831f32613b817e8e8db
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-09 16:34:55 -0300

    Refatoração: removendo o tamanho do vetor

--- capitulo_01-ja_conheco_os_problemas_mas_quem_sao_as_solucoes_os_algoritmos/produtos/src/br/com/alura/algoritmos/TestaMenorPreco.java
+++ capitulo_01-ja_conheco_os_problemas_mas_quem_sao_as_solucoes_os_algoritmos/produtos/src/br/com/alura/algoritmos/TestaMenorPreco.java
@@ -3,12 +3,13 @@ package br.com.alura.algoritmos;
 public class TestaMenorPreco {
 
 	public static void main(String[] args) {
-		Produto produtos[] = new Produto[5];
-		produtos[0] = new Produto("Lamborghini", 1000000);
-		produtos[1] = new Produto("Jipe", 46000);
-		produtos[2] = new Produto("Brasília", 16000);
-		produtos[3] = new Produto("Smart", 46000);
-		produtos[4] = new Produto("Fusca", 17000);
+		Produto produtos[] = {
+			new Produto("Lamborghini", 1000000),
+			new Produto("Jipe", 46000),
+			new Produto("Brasília", 16000),
+			new Produto("Smart", 46000),
+			new Produto("Fusca", 17000)
+		};
 		
 		int maisBarato = 0;
 		for (int atual = 0; atual <= 4; atual++) {
