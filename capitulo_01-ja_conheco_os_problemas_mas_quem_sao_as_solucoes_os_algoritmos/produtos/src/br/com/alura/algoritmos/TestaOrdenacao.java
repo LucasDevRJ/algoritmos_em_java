8fe28693594db84b98ff70ddff363915db355851
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-10 17:10:31 -0300

    Ordenação dos carros de mais barato para o mais caro

--- capitulo_01-ja_conheco_os_problemas_mas_quem_sao_as_solucoes_os_algoritmos/produtos/src/br/com/alura/algoritmos/TestaOrdenacao.java
+++ capitulo_01-ja_conheco_os_problemas_mas_quem_sao_as_solucoes_os_algoritmos/produtos/src/br/com/alura/algoritmos/TestaOrdenacao.java
@@ -10,5 +10,27 @@ public class TestaOrdenacao {
 				new Produto("Smart", 46000),
 				new Produto("Fusca", 17000)
 		};
+		
+		for (int atual = 0; atual < produtos.length; atual++) {
+			int menor = buscaMenor(produtos, atual, produtos.length - 1);
+			Produto produtoAtual = produtos[atual];
+			Produto produtoMenor = produtos[menor];
+			produtos[atual] = produtoMenor;
+			produtos[menor] = produtoAtual;
+		}
+		
+		for (Produto produto : produtos) {
+			System.out.println(produto.getNome() + " custa " + produto.getPreco());
+		}
+	}
+	
+	private static int buscaMenor(Produto[] produtos, int inicio, int termino) {
+		int maisBarato = inicio;
+		for (int atual = inicio; atual <= termino; atual++) {
+			if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
+				maisBarato = atual;
+			}
+		}
+		return maisBarato;
 	}
 }
