e0be2fca5a7da511a5f54c3057dfcb1756a7fd91
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-10-22 12:49:59 -0300

    Instancias das Notas

---
+++ capitulo_08-intercalando_baseado_em_um_unico_array/notas/src/notas/Nota.java
@@ -0,0 +1,12 @@
+package notas;
+
+public class Nota {
+
+	private String aluno;
+	private Double nota;
+	
+	public Nota(String aluno, Double nota) {
+		this.aluno = aluno;
+		this.nota = nota;
+	}
+}
