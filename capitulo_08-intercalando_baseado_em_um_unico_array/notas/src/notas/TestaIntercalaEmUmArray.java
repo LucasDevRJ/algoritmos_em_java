c29bbbb6181608f3ba2a28b34210130d6ba18811
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-10-22 12:52:44 -0300

    unindo todas as notas

--- capitulo_08-intercalando_baseado_em_um_unico_array/notas/src/notas/TestaIntercalaEmUmArray.java
+++ capitulo_08-intercalando_baseado_em_um_unico_array/notas/src/notas/TestaIntercalaEmUmArray.java
@@ -3,14 +3,11 @@ package notas;
 public class TestaIntercalaEmUmArray {
 
 	public static void main(String[] args) {
-		Nota[] notasDoAniche = {
+		Nota[] notas = {
 				new Nota("Andre", 4.0),
 				new Nota("Mariana", 5.0),
 				new Nota("Carlos", 8.5),
-				new Nota("Paulo", 9.0)
-		};
-		
-		Nota[] notasDoAlberto = {
+				new Nota("Paulo", 9.0),
 				new Nota("Jonas", 3.0),
 				new Nota("Juliana", 6.7),
 				new Nota("Guilherme", 7.0),
